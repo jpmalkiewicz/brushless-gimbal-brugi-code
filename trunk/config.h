e4743a696bec21c1dbb58dfceea94cd1213e3c9e
ludwigfaerberv1@gmail.com <ludwigfaerberv1@gmail.com@78e53593-a97e-4538-b1f1-ba46b1acb8ef>
2013-01-01 00:27:13 +0000

    - Now with MPU6050 DMP - Change in PID Controller - Add EEprom - Add Serial Read

---
+++ trunk/config.h
@@ -0,0 +1,13 @@
+// Choose Direction for the Motors , Normal (1) or Reverse (-1)  
+#define Direction_Motor_Nick          1
+#define Direction_Motor_Roll          1
+
+// PID Values for Nick and Roll
+#define P_Nick 0.2
+#define I_Nick 0.0
+#define D_Nick 0.0
+
+#define P_Roll 0.3
+#define I_Roll 0.60
+#define D_Roll 0.02
+
