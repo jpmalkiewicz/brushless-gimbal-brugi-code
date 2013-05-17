9c6ed26d9ae4c77e607371732d0fa967eec25d0b
ahahn.1.yt@gmail.com <ahahn.1.yt@gmail.com@78e53593-a97e-4538-b1f1-ba46b1acb8ef>
2013-05-17 06:57:29 +0000

    tagged release version 048

---
+++ tags/BruGi_048/definitions.h
@@ -0,0 +1,91 @@
+/*************************/
+/* Definitions           */
+/*************************/
+// MPU Address Settings
+#define MPU6050_ADDRESS_AD0_LOW     0x68 // default for InvenSense evaluation board
+#define MPU6050_ADDRESS_AD0_HIGH    0x69 // Drotek MPU breakout board
+#define MPU6050_DEFAULT_ADDRESS     MPU6050_ADDRESS_AD0_HIGH
+
+
+// Define Brushless PWM Mode, uncomment ONE setting
+#define PWM_32KHZ_PHASE  // Resolution 8 bit for PWM
+//#define PWM_8KHZ_FAST    // Resolution 8 bit for PWM
+//#define PWM_4KHZ_PHASE   // Resolution 8 bit for PWM
+//#define NO_PWM_LOOP
+
+#define MOTORUPDATE_FREQ 32 //in kHz 1,2,4,8 for 32kHz, 1,2,4 for 4kHz
+
+// Do not change for now
+#define MPU6050_GYRO_FS MPU6050_GYRO_FS_250  // +-250,500,1000,2000 deg/s
+#define MPU6050_DLPF_BW MPU6050_DLPF_BW_188  // 5,10,20,42,98,188,256 Hz
+
+
+// Number of sinus values for full 360 deg.
+// NOW FIXED TO 256 !!!
+// Reason: Fast Motor Routine using uint8_t overflow for stepping
+#define N_SIN 256
+
+#define SCALE_ACC 10000.0
+#define SCALE_PID_PARAMS 1000.0f
+
+// RC Pins
+#define RC_PIN_ROLL A2
+#define RC_PIN_PITCH A1
+#define MID_RC 1500
+#define MIN_RC 1000
+#define MAX_RC 2000
+#define RC_DEADBAND 50
+
+// I2C Frequency
+//#define I2C_SPEED 100000L     //100kHz normal mode
+//#define I2C_SPEED 400000L   //400kHz fast mode
+#define I2C_SPEED 800000L   //800kHz ultra fast mode
+
+// Hardware Abstraction for Motor connectors,
+// DO NOT CHANGE UNLES YOU KNOW WHAT YOU ARE DOING !!!
+#define PWM_A_MOTOR1 OCR2A
+#define PWM_B_MOTOR1 OCR1B
+#define PWM_C_MOTOR1 OCR1A
+
+#define PWM_A_MOTOR0 OCR0A
+#define PWM_B_MOTOR0 OCR0B
+#define PWM_C_MOTOR0 OCR2B
+
+
+#ifdef DEBUG
+    #define DEBUG_PRINT(x) Serial.print(x)
+    #define DEBUG_PRINTF(x, y) Serial.print(x, y)
+    #define DEBUG_PRINTLN(x) Serial.println(x)
+    #define DEBUG_PRINTLNF(x, y) Serial.println(x, y)
+#else
+    #define DEBUG_PRINT(x)
+    #define DEBUG_PRINTF(x, y)
+    #define DEBUG_PRINTLN(x)
+    #define DEBUG_PRINTLNF(x, y)
+#endif
+
+
+#ifdef PWM_32KHZ_PHASE
+  #define CC_FACTOR 32
+#endif
+#ifdef PWM_4KHZ_PHASE
+  #define CC_FACTOR 4
+#endif
+#ifdef PWM_8KHZ_FAST
+  #define CC_FACTOR 8
+#endif
+#ifdef NO_PWM_LOOP
+  #define CC_FACTOR 1
+#endif
+
+
+#define LEDPIN_PINMODE             pinMode (8, OUTPUT);
+#define LEDPIN_SWITCH              digitalWrite(8,!bitRead(PORTB,0));
+#define LEDPIN_OFF                 digitalWrite(8, LOW);
+#define LEDPIN_ON                  digitalWrite(8, HIGH);
+
+
+
+
+
+
