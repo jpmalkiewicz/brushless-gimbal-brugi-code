3850a85df59bb449875dbf0e26116889b4f9274c
ahahn.1.yt <ahahn.1.yt@gmail.com>
2013-05-27 21:34:41 +0000

    improved atan2 angle calculation, fixed +/-45 degree glitch (just on pitch in this version)

--- _BruGi/fastMathRoutines.h
+++ _BruGi/fastMathRoutines.h
@@ -69,6 +69,7 @@ int8_t sgn(int val) {
 }
 
 
+// TDO: quadrants
 inline int16_t _atan2x(float y, float x) {
   if (abs(x) > abs(y)) {
     return _atan2(y,x);
@@ -88,12 +89,73 @@ inline int32_t constrain_int32(int32_t x , int32_t l, int32_t h) {
   }
 }
 
+//***************************************************************
+// “Efficient approximations for the arctangent function”,
+// Rajan, S. Sichun Wang Inkol, R. Joyal, A., May 2006
+//***************************************************************
+inline float Rajan_FastArcTan(float x) {
+  return PI/4.0*x - x*(fabs(x) - 1)*(0.2447 + 0.0663*fabs(x));
+}
+
+// atan2 for all quadrants by A. Hahn
+inline float Rajan_FastArcTan2(float y, float x) {
+
+  uint8_t qCode;
+  float pi_2 = PI/2.0;
+  float q;
+  float z;
+
+  // 6 us
+  bool swap45 = (fabs(y) > fabs(x));
+    
+  // 22us
+  if ((y >= 0) && (x >= 0)) { qCode = 0; }
+  if ((y >= 0) && (x <= 0)) { qCode = 1; }
+  if ((y <= 0) && (x <= 0)) { qCode = 2; }
+  if ((y <= 0) && (x >= 0)) { qCode = 3; }
+
+  // 54 us
+  if (swap45) {
+    q = x / y;
+  } else {
+    q = y / x;
+  }
+
+  // 92 us
+  z = Rajan_FastArcTan(q);
+
+  if (swap45) {
+    switch (qCode) {
+      case 0: z = pi_2 - z;  break;
+      case 1: z = pi_2 - z;  break;
+      case 2: z = -pi_2 - z; break;
+      case 3: z = -pi_2 - z; break;
+    }
+  } else {
+    switch (qCode) {    
+      case 0: z = z;         break;
+      case 1: z = PI + z;    break;
+      case 2: z = -PI + z;   break;
+      case 3: z = z;         break;
+    }
+  }
+  
+  return z;
+}
+
+// atan2 returnig degrees * 100
+int16_t Rajan_FastArcTan2_deg100(float y, float x) {
+  return 180/PI * 100 * Rajan_FastArcTan2(y, x);
+}
+
+
 /************************/
 /* LP Filter            */
 /************************/
 inline void utilLP_float(float * q, float i, float coeff) {
   *q += (i-*q)*coeff;
 }
+
 
 
 /************************/
